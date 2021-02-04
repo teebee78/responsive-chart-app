c7cdd92961599a112475759111a819c4021a2359
Thomas Bühler <Thomas.Buehler@netcetera.com>
2021-02-04 08:45:47 +0100

    add chart component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {ChartComponent} from './chart/chart.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ChartComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
